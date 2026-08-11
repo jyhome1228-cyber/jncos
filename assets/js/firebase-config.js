9a915b5f2dd8bdf7a423d291b4ee79ea608bcc0a
JAEYOUNG PARK <jyhome1228@gmail.com>
2026-08-11 14:41:50 +0900

    Connect JN COS TECH web app to Firebase project

--- assets/js/firebase-config.js
+++ assets/js/firebase-config.js
@@ -1,13 +1,12 @@
 (() => {
-  window.JNCOS_FIREBASE_CONFIG = window.JNCOS_FIREBASE_CONFIG || {
-    apiKey: '',
-    authDomain: '',
-    projectId: '',
-    storageBucket: '',
-    messagingSenderId: '',
-    appId: ''
+  window.JNCOS_FIREBASE_CONFIG = {
+    apiKey: 'AIzaSyC-QT7LqvH4qXwhZDHDyyzV4r1y8rZTLcM',
+    authDomain: 'jncostech.firebaseapp.com',
+    projectId: 'jncostech',
+    storageBucket: 'jncostech.firebasestorage.app',
+    messagingSenderId: '629672019213',
+    appId: '1:629672019213:web:c2c0d5699fb65ce848dc44'
   };
 
-  /* Existing JN COS TECH GA4 web stream. */
-  window.JNCOS_GA4_MEASUREMENT_ID = window.JNCOS_GA4_MEASUREMENT_ID || 'G-9JRLB4KEVK';
+  window.JNCOS_GA4_MEASUREMENT_ID = 'G-9JRLB4KEVK';
 })();
