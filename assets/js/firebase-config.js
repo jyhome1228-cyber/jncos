04d6ada58fb0b5fa1a394c916b02cc08beeb603a
JAEYOUNG PARK <jyhome1228@gmail.com>
2026-08-11 11:04:48 +0900

    Add Firebase and GA4 configuration entry point

---
+++ assets/js/firebase-config.js
@@ -0,0 +1,13 @@
+(() => {
+  window.JNCOS_FIREBASE_CONFIG = window.JNCOS_FIREBASE_CONFIG || {
+    apiKey: '',
+    authDomain: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: '',
+    appId: ''
+  };
+
+  /* Existing JN COS TECH GA4 web stream. */
+  window.JNCOS_GA4_MEASUREMENT_ID = window.JNCOS_GA4_MEASUREMENT_ID || 'G-9JRLB4KEVK';
+})();
